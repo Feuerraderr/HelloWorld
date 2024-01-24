4a3772ae2a8aee209105b33b2399e941288f36a0
Feuerraderr <belov-vlad2008@yandex.ru>
2024-01-24 19:57:04 +0300

    Prepare project

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  HelloWorld
-//
-//  Created by Владислав Резван on 24.01.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
